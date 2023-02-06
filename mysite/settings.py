11863d2972d66db777457d56abb761525b333ff5
GAEUL MOON <103443650+gaeull@users.noreply.github.com>
2023-02-06 19:44:49 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5awyaxyg@p&e!hwy3a5_8z^svzqz7sw+o+xe*8brd2xz&x#@%q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True --> 개발모드
